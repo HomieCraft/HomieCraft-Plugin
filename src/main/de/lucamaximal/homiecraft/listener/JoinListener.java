de0df1f8de77a4c5b79e228ebae1accd531478f3
HomieCraft <luca.golbs@icloud.com>
2026-03-16 19:12:58 +0100

    Update JoinListener.java

--- src/main/de/lucamaximal/homiecraft/listener/JoinListener.java
+++ src/main/de/lucamaximal/homiecraft/listener/JoinListener.java
@@ -1,13 +1,31 @@
-@EventHandler
-public void onJoin(PlayerJoinEvent event) {
+package de.lucamaximal.homiecraft.listener;
 
-    if(!event.getPlayer().hasPlayedBefore()) {
+import de.lucamaximal.homiecraft.util.MessageUtils;
+import de.lucamaximal.homiecraft.util.Messages;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
-        MessageUtils.send(event.getPlayer(), Messages.FIRST_JOIN);
-        return;
+public class JoinListener implements Listener {
 
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+
+        if (!event.getPlayer().hasPlayedBefore()) {
+            // First Join Message
+            MessageUtils.send(event.getPlayer(), Messages.FIRST_JOIN);
+            return;
+        }
+
+        // Normal Join Message
+        MessageUtils.send(event.getPlayer(), Messages.JOIN);
     }
 
-    MessageUtils.send(event.getPlayer(), Messages.JOIN);
+    @EventHandler
+    public void onQuit(org.bukkit.event.player.PlayerQuitEvent event) {
 
+        // Leave Message
+        MessageUtils.send(event.getPlayer(), Messages.LEAVE);
+    }
 }
