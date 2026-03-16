25d84e7b5fdc1995c313583dd2dd15b0fb83968b
HomieCraft <luca.golbs@icloud.com>
2026-03-16 19:09:34 +0100

    Update MessageUtils.java

--- src/main/de/lucamaximal/homiecraft/util/MessageUtils.java
+++ src/main/de/lucamaximal/homiecraft/util/MessageUtils.java
@@ -4,28 +4,52 @@ import de.lucamaximal.homiecraft.Main;
 import me.clip.placeholderapi.PlaceholderAPI;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class MessageUtils {
 
     private static final MiniMessage miniMessage = MiniMessage.miniMessage();
 
-    public static Component getMessage(String path, Player player) {
+    public static Component get(String path, Player player) {
 
         String prefix = Main.getInstance().getConfig().getString(Messages.PREFIX);
         String message = Main.getInstance().getConfig().getString(path);
 
-        if (prefix == null) prefix = "";
-        if (message == null) message = "";
+        if(prefix == null) prefix = "";
+        if(message == null) message = "";
 
         String finalMessage = prefix + message;
 
-        // PlaceholderAPI Support
-        if (player != null && Main.getInstance().getServer().getPluginManager().getPlugin("PlaceholderAPI") != null) {
+        if(player != null &&
+                Main.getInstance().getServer().getPluginManager().getPlugin("PlaceholderAPI") != null) {
+
             finalMessage = PlaceholderAPI.setPlaceholders(player, finalMessage);
         }
 
         return miniMessage.deserialize(finalMessage);
+    }
+
+    public static void send(CommandSender sender, String path) {
+
+        if(sender instanceof Player player) {
+
+            player.sendMessage(get(path, player));
+
+        } else {
+
+            String prefix = Main.getInstance().getConfig().getString(Messages.PREFIX);
+            String message = Main.getInstance().getConfig().getString(path);
+
+            if(prefix == null) prefix = "";
+            if(message == null) message = "";
+
+            sender.sendMessage(
+                    miniMessage.deserialize(prefix + message)
+            );
+
+        }
+
     }
 
 }
