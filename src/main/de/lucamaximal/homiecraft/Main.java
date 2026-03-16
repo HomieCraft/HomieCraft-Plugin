a9a544534de074da4c0e259ecdd2660a39527dec
HomieCraft <luca.golbs@icloud.com>
2026-03-16 06:39:24 +0100

    Create Main.java

---
+++ src/main/de/lucamaximal/homiecraft/Main.java
@@ -0,0 +1,16 @@
+package de.lucamaximal.homiecraft;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("Plugin wurde gestartet!");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("Plugin wurde gestoppt!");
+    }
+}
