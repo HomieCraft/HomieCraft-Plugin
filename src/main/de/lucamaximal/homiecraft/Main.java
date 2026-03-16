ed8820f99b891b1d570403d6ec00134b7484681a
HomieCraft <luca.golbs@icloud.com>
2026-03-16 06:57:39 +0100

    Update Main.java

--- src/main/de/lucamaximal/homiecraft/Main.java
+++ src/main/de/lucamaximal/homiecraft/Main.java
@@ -1,16 +1,22 @@
 package de.lucamaximal.homiecraft;
 
+import de.lucamaximal.homiecraft.listener.JoinListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
+
         getLogger().info("HomieCraft Plugin gestartet!");
+
+        getServer().getPluginManager().registerEvents(new JoinListener(), this);
+
     }
 
     @Override
     public void onDisable() {
         getLogger().info("HomieCraft Plugin gestoppt!");
     }
+
 }
