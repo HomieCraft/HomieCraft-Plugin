15cd82ef8d1d09a3b4fbae58e00fe78bf615b98a
HomieCraft <luca.golbs@icloud.com>
2026-03-16 16:12:54 +0100

    Update Messages.java

--- src/main/de/lucamaximal/homiecraft/util/Messages.java
+++ src/main/de/lucamaximal/homiecraft/util/Messages.java
@@ -2,8 +2,8 @@ package de.lucamaximal.homiecraft.util;
 
 public class Messages {
 
-    public static final String PREFIX = "§8[§b§lHomieCraft§8] §7";
+    public static final String PREFIX = "messages.prefix";
 
-    public static final String JOIN_MESSAGE = PREFIX + "§aWillkommen auf HomieCraft, §e%player%§a!";
+    public static final String JOIN_MESSAGE = "messages.join";
 
 }
