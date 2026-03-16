a7047412d9dcac3c30c40d8a2442eb096e22dfd3
HomieCraft <luca.golbs@icloud.com>
2026-03-16 19:03:07 +0100

    Update Main.java

--- src/main/de/lucamaximal/homiecraft/Main.java
+++ src/main/de/lucamaximal/homiecraft/Main.java
@@ -1,11 +1,18 @@
 package de.lucamaximal.homiecraft;
 
+import de.lucamaximal.homiecraft.commands.CommandManager;
 import de.lucamaximal.homiecraft.listener.JoinListener;
+import de.lucamaximal.homiecraft.manager.ConfigManager;
+import de.lucamaximal.homiecraft.manager.MessageManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
 
     private static Main instance;
+
+    private ConfigManager configManager;
+    private MessageManager messageManager;
+    private CommandManager commandManager;
 
     @Override
     public void onEnable() {
@@ -14,14 +21,14 @@ public final class Main extends JavaPlugin {
 
         saveDefaultConfig();
 
+        configManager = new ConfigManager(this);
+        messageManager = new MessageManager(this);
+        commandManager = new CommandManager(this);
+
         registerListeners();
+        commandManager.registerCommands();
 
         getLogger().info("HomieCraft Plugin gestartet!");
-    }
-
-    @Override
-    public void onDisable() {
-        getLogger().info("HomieCraft Plugin gestoppt!");
     }
 
     private void registerListeners() {
@@ -31,4 +38,13 @@ public final class Main extends JavaPlugin {
     public static Main getInstance() {
         return instance;
     }
+
+    public ConfigManager getConfigManager() {
+        return configManager;
+    }
+
+    public MessageManager getMessageManager() {
+        return messageManager;
+    }
+
 }
