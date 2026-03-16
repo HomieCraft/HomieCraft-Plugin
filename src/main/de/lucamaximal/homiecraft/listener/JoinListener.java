2ecfc4c21558f222f383b32cf789cb30943c876c
HomieCraft <luca.golbs@icloud.com>
2026-03-16 06:56:56 +0100

    Update JoinListener.java

--- src/main/de/lucamaximal/homiecraft/listener/JoinListener.java
+++ src/main/de/lucamaximal/homiecraft/listener/JoinListener.java
@@ -1,1 +1,20 @@
+package de.lucamaximal.homiecraft.listener;
 
+import de.lucamaximal.homiecraft.util.Messages;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class JoinListener implements Listener {
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+
+        String message = Messages.JOIN_MESSAGE
+                .replace("%player%", event.getPlayer().getName());
+
+        event.getPlayer().sendMessage(message);
+
+    }
+
+}
