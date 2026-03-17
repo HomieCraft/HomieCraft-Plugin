fb4dce0df2b99b713a728687f3f4f6bff2717ecb
HomieCraft <luca.golbs@icloud.com>
2026-03-17 16:40:21 +0100

    Create SpawnSupCommand.java

---
+++ src/main/de/lucamaximal/homiecraft/commands/general/subcommands/SpawnSupCommand.java
@@ -0,0 +1,56 @@
+package de.lucamaximal.homiecraft.commands.general.subcommands;
+
+import de.lucamaximal.homiecraft.core.Main;
+import org.bukkit.Location;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class SpawnSubCommand implements SubCommand {
+
+    private final Main plugin;
+
+    public SpawnSubCommand(Main plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public String getName() {
+        return "spawn";
+    }
+
+    @Override
+    public String getPermission() {
+        return "homiecraft.spawn";
+    }
+
+    @Override
+    public void execute(CommandSender sender, String[] args) {
+
+        if (!(sender instanceof Player player)) {
+            sender.sendMessage("Only players!");
+            return;
+        }
+
+        if (!player.hasPermission(getPermission())) {
+            player.sendMessage(plugin.getMessageManager().getMessage("no_permission"));
+            return;
+        }
+
+        if (!plugin.getConfig().contains("spawn")) {
+            player.sendMessage(plugin.getMessageManager().getMessage(player, "no_spawn_set"));
+            return;
+        }
+
+        Location loc = new Location(
+                plugin.getServer().getWorld(plugin.getConfig().getString("spawn.world")),
+                plugin.getConfig().getDouble("spawn.x"),
+                plugin.getConfig().getDouble("spawn.y"),
+                plugin.getConfig().getDouble("spawn.z"),
+                (float) plugin.getConfig().getDouble("spawn.yaw"),
+                (float) plugin.getConfig().getDouble("spawn.pitch")
+        );
+
+        player.teleport(loc);
+        player.sendMessage(plugin.getMessageManager().getMessage(player, "spawn_teleport"));
+    }
+}
