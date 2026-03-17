bfd39f7c3cf61318ccbcac72a262e60c1fe6bf87
HomieCraft <luca.golbs@icloud.com>
2026-03-17 15:28:27 +0100

    Update Main.java

--- src/main/de/lucamaximal/homiecraft/core/Main.java
+++ src/main/de/lucamaximal/homiecraft/core/Main.java
@@ -3,6 +3,7 @@ package de.lucamaximal.homiecraft.core;
 import de.lucamaximal.homiecraft.config.MessageManager;
 import de.lucamaximal.homiecraft.listener.JoinListener;
 import de.lucamaximal.homiecraft.listener.LeaveListener;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
@@ -15,10 +16,8 @@ public final class Main extends JavaPlugin {
 
         instance = this;
 
-        // Config laden
         saveDefaultConfig();
 
-        // MessageManager initialisieren
         messageManager = new MessageManager(getConfig());
 
         // Listener registrieren
@@ -30,20 +29,19 @@ public final class Main extends JavaPlugin {
                 new LeaveListener(messageManager), this
         );
 
-        getLogger().info("HomieCraft Plugin wurde gestartet!");
+        // Plugin Start Message (für Konsole + Server)
+        Bukkit.getConsoleSender().sendMessage(messageManager.getMessage("plugin_enable"));
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("HomieCraft Plugin wurde gestoppt!");
+        Bukkit.getConsoleSender().sendMessage(messageManager.getMessage("plugin_disable"));
     }
 
-    // Zugriff auf Instanz (optional aber praktisch)
     public static Main getInstance() {
         return instance;
     }
 
-    // Zugriff auf MessageManager
     public MessageManager getMessageManager() {
         return messageManager;
     }
