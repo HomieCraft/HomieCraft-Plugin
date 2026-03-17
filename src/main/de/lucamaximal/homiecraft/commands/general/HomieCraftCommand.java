2c7702881e67ffba1982e1dce0fd6251efdfb743
HomieCraft <luca.golbs@icloud.com>
2026-03-17 16:09:13 +0100

    Update HomieCraftCommand.java

--- src/main/de/lucamaximal/homiecraft/commands/general/HomieCraftCommand.java
+++ src/main/de/lucamaximal/homiecraft/commands/general/HomieCraftCommand.java
@@ -1,6 +1,5 @@
 package de.lucamaximal.homiecraft.commands.general;
 
-import de.lucamaximal.homiecraft.config.MessageManager;
 import de.lucamaximal.homiecraft.core.Main;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -9,27 +8,25 @@ import org.bukkit.command.CommandSender;
 public class HomieCraftCommand implements CommandExecutor {
 
     private final Main plugin;
-    private final MessageManager messageManager;
 
     public HomieCraftCommand(Main plugin) {
         this.plugin = plugin;
-        this.messageManager = plugin.getMessageManager();
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
-        // /homiecraft reload
+        // /hc reload
         if (args.length == 1 && args[0].equalsIgnoreCase("reload")) {
 
-            plugin.reloadConfig();
+            plugin.reloadPlugin();
 
-            sender.sendMessage(messageManager.getMessage("plugin_enable")); // kannst du später ändern
+            sender.sendMessage(plugin.getMessageManager().getMessage("plugin_enable"));
             return true;
         }
 
-        // Default / Hilfe
-        sender.sendMessage(messageManager.getMessage("help"));
+        // Default (Help)
+        sender.sendMessage(plugin.getMessageManager().getMessage("help"));
         return true;
     }
 }
