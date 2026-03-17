8f01170d5ada786863debd7357b3b68666c0a604
HomieCraft <luca.golbs@icloud.com>
2026-03-17 20:57:02 +0100

    Create TpaAutoCommand.java

---
+++ src/main/de/lucamaximal/homiecraft/commands/tpa/TpaAutoCommand.java
@@ -0,0 +1,24 @@
+package de.lucamaximal.homiecraft.commands.tpa;
+
+import de.lucamaximal.homiecraft.tpa.TpaManager;
+import org.bukkit.command.*;
+import org.bukkit.entity.Player;
+
+public class TpaAutoCommand implements CommandExecutor {
+
+    private final TpaManager manager;
+
+    public TpaAutoCommand(TpaManager manager) {
+        this.manager = manager;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if (sender instanceof Player player) {
+            manager.toggleAuto(player);
+        }
+
+        return true;
+    }
+}
