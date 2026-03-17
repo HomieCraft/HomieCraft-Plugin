fdc2fa1fa5e635f7cc79437b606af443f3ed6bb5
HomieCraft <luca.golbs@icloud.com>
2026-03-17 16:33:42 +0100

    Create SubCommand.java

---
+++ src/main/de/lucamaximal/homiecraft/commands/general/subcommands/SubCommand.java
@@ -0,0 +1,12 @@
+package de.lucamaximal.homiecraft.commands.general.subcommands;
+
+import org.bukkit.command.CommandSender;
+
+public interface SubCommand {
+
+    String getName();
+
+    String getPermission();
+
+    void execute(CommandSender sender, String[] args);
+}
