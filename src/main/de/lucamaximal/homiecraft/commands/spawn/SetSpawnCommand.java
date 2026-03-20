86cebc58e693b04c5b307e23c1222a5531196fb8
HomieCraft <luca.golbs@icloud.com>
2026-03-20 07:02:27 +0100

    Update SetSpawnCommand.java

--- src/main/de/lucamaximal/homiecraft/commands/spawn/SetSpawnCommand.java
+++ src/main/de/lucamaximal/homiecraft/commands/spawn/SetSpawnCommand.java
@@ -19,7 +19,6 @@ public class SetSpawnCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-
         if (!(sender instanceof Player player)) {
             sender.sendMessage("Only players!");
             return true;
